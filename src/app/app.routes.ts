da0c8ea35b5801ceee529189fce388ef86ed1067
RobinsonHCastillo <rcastilloi3@upao.edu.pe>
2024-08-29 14:05:05 -0500

    setup

---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/login/login.component';
+
+export const routes: Routes = [{path:"login" , component: LoginComponent}];
